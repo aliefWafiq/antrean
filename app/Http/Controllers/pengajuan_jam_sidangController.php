b7be9d9198dc1078b3ef9ba70168874924c24b39
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-10-29 07:12:03 +0700

    update

--- app/Http/Controllers/pengajuan_jam_sidangController.php
+++ app/Http/Controllers/pengajuan_jam_sidangController.php
@@ -13,10 +13,43 @@ class pengajuan_jam_sidangController extends Controller
 
     public function ubahJamSidang()
     {
-        $antreanId = Auth::id();
-        $dataAntrean = antreans::where('id', $antreanId)->latest()->first();
+        $perkaraId = session()->get('perkara_id');
+        $dataAntrean = antreans::where('id_perkara', $perkaraId)->latest()->first();
 
-        return view('ubahJam', ['dataAntrean' => $dataAntrean]);
+        if (!$dataAntrean) {
+            return redirect('/antrean')->with('error', 'Anda harus memiliki antrean terlebih dahulu.');
+        }
+
+        $allSlots = [
+            '08:00',
+            '08:30',
+            '09:00',
+            '09:30',
+            '10:00',
+            '10:30',
+            '11:00',
+            '11:30',
+            '13:30',
+            '14:00',
+            '14:30' 
+        ];
+
+        $tanggalSidang = $dataAntrean->tanggal_sidang;
+
+        $takenSlots = antreans::where('tanggal_sidang', $tanggalSidang)
+            ->whereNotNull('jam_perkiraan') 
+            ->pluck('jam_perkiraan')    
+            ->map(function ($time) {
+                return Carbon::parse($time)->format('H:i');
+            })
+            ->all();
+
+        $availableSlots = array_diff($allSlots, $takenSlots);
+
+        return view('ubahJam', [ 
+            'dataAntrean' => $dataAntrean,
+            'availableSlots' => $availableSlots
+        ]);
     }
 
     public function editJamSidang(Request $request, antreans $antrean)
@@ -25,9 +58,11 @@ class pengajuan_jam_sidangController extends Controller
             'slotJamTersedia' => 'required|date_format:H:i'
         ]);
 
+        $jamPilihan = $request->slotJamTersedia . ':00';
+
         pengajuanJamSidangs::create([
             'id_user' => $antrean->id,
-            'jam_sidang' => $validated['slotJamTersedia']
+            'jam_sidang' => $jamPilihan
         ]);
 
         return redirect('/antrean')->with('showModal', true);
